4036d1990cc71c3c99ff076121b6798521bd8f40
chandragupt553 <63948886+chandragupt553@users.noreply.github.com>
2022-06-24 13:36:04 +0530

    initial comment

---
+++ assignment1.c
@@ -0,0 +1,20 @@
+#include <stdio.h> //Assignment1 Q-1,2,3,4.
+void main()
+{
+   printf("Chandragupt\n");
+   printf("Natratj Society,1\n");
+   printf("Bharuch,393120\n");
+   
+   int x;
+   printf("Enter any number\n");
+   scanf("%d",&x);
+   
+   int y;
+   printf("Enter any number\n");
+   scanf("%d",&y);
+   
+   int z;
+   z=x+y;
+   printf("sum of enterd numbers: %d\n",z);  
+   }
+   
